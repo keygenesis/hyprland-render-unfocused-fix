d214324724745bc1d5bce6b3dff575032d6e640a
david <david@david.rip>
2026-04-28 18:07:27 -0400

    edit flake.nix to include hyprland-dev

--- flake.nix
+++ flake.nix
@@ -6,18 +6,20 @@
     hyprland.url = "github:hyprwm/Hyprland";
   };
 
-  outputs = { self, nixpkgs, hyprland }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
-      packages.${system}.default = pkgs.callPackage ./default.nix {
-        hyprland = hyprland.packages.${system}.hyprland;
-      };
-
-      devShells.${system}.default = pkgs.mkShell {
-        inputsFrom = [ self.packages.${system}.default ];
-      };
+  outputs = {
+    self,
+    nixpkgs,
+    hyprland,
+  }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    packages.${system}.default = pkgs.callPackage ./default.nix {
+      hyprland = hyprland.packages.${system}.hyprland-dev;
     };
+
+    devShells.${system}.default = pkgs.mkShell {
+      inputsFrom = [self.packages.${system}.default];
+    };
+  };
 }
