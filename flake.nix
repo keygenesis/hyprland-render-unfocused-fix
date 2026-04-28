4c26a7102b9997a853c7784001c3d89597c212dc
david <david@david.rip>
2026-04-28 17:45:11 -0400

    added flake.nix

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Hyprland plugin that performs offscreen rendering for selected windows without drawing any visible overlay on screen";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    hyprland.url = "github:hyprwm/Hyprland";
+  };
+
+  outputs = { self, nixpkgs, hyprland }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      packages.${system}.default = pkgs.callPackage ./default.nix {
+        hyprland = hyprland.packages.${system}.hyprland;
+      };
+
+      devShells.${system}.default = pkgs.mkShell {
+        inputsFrom = [ self.packages.${system}.default ];
+      };
+    };
+}
